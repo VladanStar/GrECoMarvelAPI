da7e5086fdb468fcb382a3e0c8514b5d8640987c
unknown <vladancupric@gmail.com>
2022-02-09 11:09:20 +0100

    New commits

--- src/components/MainPage/SearchBar/SearchBar.js
+++ src/components/MainPage/SearchBar/SearchBar.js
@@ -4,15 +4,15 @@ import style from './SearchBar.module.css';
 const SearchBar = ({ searchHeroes }) => {
 
     const search = (event) => {
-        if (event.charCode === 13) {
+       
             const text = event.target.value;
             searchHeroes(text)
-        }
+        
     }
     return (
         <>
         <div className={style.margin}>
-            <input className="form-control mr-sm-2" type="search" placeholder="Type hero and press enter" aria-label="Search" onKeyPress={search} />
+            <input className="form-control mr-sm-2" type="search" placeholder="Type hero and press enter" aria-label="Search" onInput={search} />
         </div>
         </>
     )
